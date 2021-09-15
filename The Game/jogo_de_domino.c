06a7222266e6e7a5fa66ab491746ab16f16368ef
GuilhermeGuimaraes01 <89594705+GuilhermeGuimaraes01@users.noreply.github.com>
2021-09-14 22:55:07 -0300

    Update jogo_de_domino.c

--- The Game/jogo_de_domino.c
+++ The Game/jogo_de_domino.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
+    int x;
+    int y;
+    scanf(x,y);
+    if(x == y){
+    printf("peça correta")
+    }
+    else{
+    printf("peça errada, coloque outra."
+    }
     return 0;
 }
